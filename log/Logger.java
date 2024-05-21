3823d857cc8194f177d45c0b6529cc97e16a089d
sergio <sergio@git.com>
2024-05-22 06:19:14 +0800

    the first commitment

---
+++ log/Logger.java
@@ -0,0 +1,19 @@
+package log;
+import java.time.*;
+public class Logger{
+	private ZoneId zone;
+	private Logger(String zoneId){
+		this.zone = ZoneId.of(zondId);
+	}
+	public Logger getLogger(){
+		return new Logger("GMT+08:00");
+	}
+	public Logger getLogger(String zoneId){
+		return new Logger(zoneId);
+	}
+	public void log(String msg){
+		var builder = new StringBuilder();
+		builder.append("[");
+		builder.append(Local
+	}
+}
