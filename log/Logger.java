3189ca80a1c675a2400ccd951461cb0032766426
sergio <sergio@git.com>
2024-05-25 21:56:44 +0800

    the second version

--- log/Logger.java
+++ log/Logger.java
@@ -1,19 +1,47 @@
 package log;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.time.*;
 public class Logger{
-	private ZoneId zone;
+	private final ZoneId zone;
 	private Logger(String zoneId){
-		this.zone = ZoneId.of(zondId);
+		this.zone = ZoneId.of(zoneId);
 	}
-	public Logger getLogger(){
+	public static Logger getLogger(){
 		return new Logger("GMT+08:00");
 	}
-	public Logger getLogger(String zoneId){
+	public static Logger getLogger(String zoneId){
 		return new Logger(zoneId);
 	}
+	private PrintWriter out = null;
 	public void log(String msg){
-		var builder = new StringBuilder();
-		builder.append("[");
-		builder.append(LocalTime.of(zone));
+		var builder = genTime();
+		builder.append(Thread.currentThread().getName());
+		builder.append("/INFO ");
+		builder.append(msg);
+		if(out == null) System.out.println(builder);
+		else out.println(builder);
+	}
+	public void setOut(OutputStream os){
+		this.out = new PrintWriter(os);
+	}
+	private StringBuilder genTime(){
+		var str = new StringBuilder("[");
+		var time = LocalTime.now(zone);
+		str.append(time.getHour());
+		str.append(":");
+		str.append(time.getMinute());
+		str.append(":");
+		str.append(time.getSecond());
+		str.append("] ");
+		return str;
+	}
+	public void err(String msg, Throwable throwable){
+		var builder = genTime();
+		builder.append(Thread.currentThread().getName());
+		builder.append("/ERROR ");
+		builder.append(msg);
+		System.err.print(builder);
+		throwable.printStackTrace(System.err);
 	}
 }
